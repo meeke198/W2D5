733047c272de6ea46d8dbe1f5fbfb9c7061ddc80
hien nguyen <thuhiennguyen198@gmail.com>
2021-07-14 22:58:16 -0400

    Finish

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -1,5 +1,5 @@
 class Flight
-    attr_reader :flight_number_string, :capacity
+    attr_reader :flight_number, :capacity #tai sao trong dap an chi set passenger nhung van pass all spec, con file minh y chang ma ko khai flight_number, capacity laf khong chay duoc.
     def initialize(flight_number, capacity)
         @flight_number = flight_number
         @capacity = capacity
@@ -20,9 +20,20 @@ class Flight
     end
 
     def board_passenger(passenger)
-        passenger.passengers.full?
+     if passenger.has_flight?(@flight_number) && !self.full? #tai sao passenger la Passenger instance ma khong phai la passenger.flight_number trong ngoac la arg ma lai xai flight_number duoc luon?
+        @passengers << passenger
+     end
     end
 
+    def list_passengers
+        @passengers.map {|passenger| passenger.name}
+    end
 
+    def [](index)
+        @passengers[index]
+    end
 
+    def <<(passenger)
+        self.board_passenger(passenger)
+    end
 end
