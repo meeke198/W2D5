f01cdf6fdfa4327a482a555756891278b5bfc5c1
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-20 15:29:51 -0400

    Passenger class

--- flight_project/lib/passenger.rb
+++ flight_project/lib/passenger.rb
@@ -1,24 +1,16 @@
 
 class Passenger
     attr_reader :name
+    
     def initialize(name)
         @name = name
-        @flight_numbers  = []
+        @flight_numbers = []
+    end
+    def has_flight?(flight_number)
+        @flight_numbers.include?(flight_number.upcase) ? true : false
+    end
+    def add_flight(flight_number)
+        @flight_numbers << flight_number.upcase if !self.has_flight?(flight_number.upcase)        
     end
 
-    def has_flight?(flight_number_string)
-        # debugger
-        if @flight_numbers.include?(flight_number_string.upcase)
-            return true
-        else
-            false
-        end
-    end
-
-    def add_flight(flight_number_string)
-        if !self.has_flight?(flight_number_string)
-            @flight_numbers << flight_number_string.upcase
-        end
-
-    end
 end
