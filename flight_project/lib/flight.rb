eb50985a6eae102042ffbcc9e7a1b563a05a8384
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-20 15:50:02 -0400

    define Flight class

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -1,30 +1,24 @@
+
 class Flight
-    attr_reader :flight_number, :capacity #tai sao trong dap an chi set passenger nhung van pass all spec, con file minh y chang ma ko khai flight_number, capacity laf khong chay duoc.
     def initialize(flight_number, capacity)
         @flight_number = flight_number
         @capacity = capacity
-        @passengers = []
+        @passengers = []    
     end
 
     def passengers
-        @passengers
+        @passengers 
     end
-    
 
     def full?
-        if @passengers.length == capacity
-            return true
-        else
-            return false
-        end
+        @passengers.length == @capacity
     end
 
     def board_passenger(passenger)
-     if passenger.has_flight?(@flight_number) && !self.full? #tai sao passenger la Passenger instance ma khong phai la passenger.flight_number trong ngoac la arg ma lai xai flight_number duoc luon?
-        @passengers << passenger
-     end
+        if passenger.has_flight?(@flight_number) && !self.full?
+            @passengers << passenger 
+        end
     end
-
     def list_passengers
         @passengers.map {|passenger| passenger.name}
     end
